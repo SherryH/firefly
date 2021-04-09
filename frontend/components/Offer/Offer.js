cba39d9a7d2a8d9ebf8806bad9f952e14e54f983
SherryH <enjoy.wind7rain@gmail.com>
2021-04-09 13:00:52 +0200

    Add hover style to offer

--- frontend/components/Offer/Offer.js
+++ frontend/components/Offer/Offer.js
@@ -1,7 +1,14 @@
 import React from 'react';
 import { Tag } from '@chakra-ui/react';
+import { css } from '@emotion/react';
 
 const OfferComponent = ({ id, children, ...rest }, ref) => {
+  const hoverCss = css`
+    &:focus, &:hover,&:active{
+      font-weight: 700;
+      box-shadow: 4px 4px 4px rgba(68, 68, 68, 0.1);
+    }
+  `
   return (
     <Tag
       key={id}
@@ -9,9 +16,11 @@ const OfferComponent = ({ id, children, ...rest }, ref) => {
       px="24px"
       py="24px"
       borderRadius="30px"
+      color="brand.900"
       bgColor="bg.100"
       w="100%"
       maxH="1.5rem"
+      css={hoverCss}
       {...rest}
       ref={ref}
     >
